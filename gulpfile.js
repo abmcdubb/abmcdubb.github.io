4e4c4cf25e43fd2d76d9a4258fe05887b4f75bf6
abmcdubb <abmcwilliams@gmail.com>
2016-11-12 15:26:25 -0500

    add scrolling nav bar and autoprefixr, fix blog page

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var sourcemaps = require('gulp-sourcemaps');
+var autoprefixer = require('gulp-autoprefixer');
 
 // Gulp Sass Task 
 gulp.task('sass', function() {
@@ -8,6 +9,9 @@ gulp.task('sass', function() {
     .pipe(sourcemaps.init())
     .pipe(sass({
       errLogToConsole: true
+    }))
+    .pipe(autoprefixer({
+      browsers: ['last 3 versions']
     }))
     .pipe(sourcemaps.write())
     .pipe(gulp.dest('./css'));
